93149f8c7de1cf46de2925482d12f0063ecec0e0
Isaac <isaac.silva1478@gmail.com>
2026-03-01 17:13:52 -0300

    feat(ServiceImp): implementando ServiceImp.java para todos os service

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/CheckoutServiceImp.java
@@ -0,0 +1,8 @@
+package com.isaac.br.selfcheckoutmercado.service.imp;
+
+import com.isaac.br.selfcheckoutmercado.service.CheckoutService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImp implements CheckoutService {
+}
---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/ProductServiceImp.java
@@ -0,0 +1,8 @@
+package com.isaac.br.selfcheckoutmercado.service.imp;
+
+import com.isaac.br.selfcheckoutmercado.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImp implements ProductService {
+}
