34ce950869a63dd0579ef36e7aa7cca8c5a1f578
Isaac <isaac.silva1478@gmail.com>
2026-02-28 21:07:58 -0300

    commit initial

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/SelfCheckoutMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.br.selfcheckoutmercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfCheckoutMercadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfCheckoutMercadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isaac/br/selfcheckoutmercado/SelfCheckoutMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.br.selfcheckoutmercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfCheckoutMercadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
