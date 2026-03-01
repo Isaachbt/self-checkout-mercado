a99b0e2ca45e5feceaea88f22ed8404250d4537d
Isaac <isaac.silva1478@gmail.com>
2026-03-01 16:47:54 -0300

    feat(Repository): implementando repository para todas entidades

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.isaac.br.selfcheckoutmercado.repository;
+
+import com.isaac.br.selfcheckoutmercado.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+}
