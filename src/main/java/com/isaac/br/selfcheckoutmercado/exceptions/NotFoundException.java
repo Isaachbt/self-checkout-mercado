68e4998eb3008797705056a3f3ec92498232ea92
Isaac <isaac.silva1478@gmail.com>
2026-03-02 20:52:27 -0300

    feat(NotFoundException): exception criada ao ter dados não encontrado

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.isaac.br.selfcheckoutmercado.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
