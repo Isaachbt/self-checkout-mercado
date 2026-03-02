20d5d9a4f29bbb5ad7823da3e92b373210810054
Isaac <isaac.silva1478@gmail.com>
2026-03-02 20:54:09 -0300

    feat(ProductServiceImp):Metodo buscando produto por barCode, criado no service

--- src/main/java/com/isaac/br/selfcheckoutmercado/service/ProductService.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/ProductService.java
@@ -1,4 +1,8 @@
 package com.isaac.br.selfcheckoutmercado.service;
 
+import com.isaac.br.selfcheckoutmercado.model.Product;
+
 public interface ProductService {
+
+    Product getBarCode(String barCode);
 }
--- src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/ProductServiceImp.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/ProductServiceImp.java
@@ -1,8 +1,27 @@
 package com.isaac.br.selfcheckoutmercado.service.imp;
 
+import com.isaac.br.selfcheckoutmercado.exceptions.NotFoundException;
+import com.isaac.br.selfcheckoutmercado.model.Product;
+import com.isaac.br.selfcheckoutmercado.repository.ProductRepository;
 import com.isaac.br.selfcheckoutmercado.service.ProductService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductServiceImp implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product getBarCode(String barCode) {
+        Optional<Product> optional = productRepository.findByBarCode(barCode);
+        if (optional.isEmpty()) {
+            throw new NotFoundException("Item não encontrado.");
+        }
+        return optional.get();
+    }
 }
