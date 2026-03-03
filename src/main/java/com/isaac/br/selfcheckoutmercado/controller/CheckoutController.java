b1e7c5001bdc86ae566ebf7851194ee73808212a
Isaac <isaac.silva1478@gmail.com>
2026-03-02 21:59:48 -0300

    feat(CheckoutController):endpoint cancelSession criada.

--- src/main/java/com/isaac/br/selfcheckoutmercado/controller/CheckoutController.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/controller/CheckoutController.java
@@ -1,15 +1,11 @@
 package com.isaac.br.selfcheckoutmercado.controller;
 
-import com.isaac.br.selfcheckoutmercado.dto.CheckoutDTO;
 import com.isaac.br.selfcheckoutmercado.dto.CheckoutResponseDTO;
 import com.isaac.br.selfcheckoutmercado.service.CheckoutService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/checkout/")
@@ -18,9 +14,15 @@ public class CheckoutController {
     @Autowired
     private CheckoutService checkoutService;
 
-
     @PostMapping("sessions")
     public ResponseEntity<CheckoutResponseDTO> createSession(){
         return ResponseEntity.ok(checkoutService.createCheckout());
+    }
+
+    @PostMapping("sessions/{id}/cancel")
+    public ResponseEntity<Object> cancelSession(@PathVariable("id") long checkoutId){
+        checkoutService.cancelCheckout(checkoutId);
+        return ResponseEntity.status(HttpStatus.OK).body("Volte sempre.");
+
     }
 }
