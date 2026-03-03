c545f38d8e5b8dce19d7631a1245053909ee3760
Isaac <isaac.silva1478@gmail.com>
2026-03-02 21:11:38 -0300

    fix(data.sql): erro 'UNIT ' arrumado 'UNIT'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO product (name, bar_code, price, price_type) VALUES
 ('Arroz Tipo 1 5kg', '7891000100103', 24.90, 'UNIT'),
 ('Feijão Carioca 1kg', '7891000200202', 8.49, 'UNIT'),
 ('Açúcar Refinado 1kg', '7891000300301', 4.79, 'UNIT'),
-('Leite Integral 1L', '7891000400400', 5.39, 'UNIT '),
+('Leite Integral 1L', '7891000400400', 5.39, 'UNIT'),
 ('Óleo de Soja 900ml', '7891000500509', 7.89, 'UNIT'),
 ('Café Torrado e Moído 500g', '7891000600608', 16.90, 'UNIT'),
 ('Carne Bovina Alcatra', '7891000700707', 39.90, 'WEIGHT'),
