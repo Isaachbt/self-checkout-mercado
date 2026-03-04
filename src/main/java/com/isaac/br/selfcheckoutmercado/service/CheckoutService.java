e68927bf1026d88fc9c71c6bb7f4e51f1f719c34
Isaac <isaac.silva1478@gmail.com>
2026-03-03 22:34:48 -0300

    feat(CheckoutService): metodo para addItemCart criado na interface

--- src/main/java/com/isaac/br/selfcheckoutmercado/service/CheckoutService.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/CheckoutService.java
@@ -1,11 +1,12 @@
 package com.isaac.br.selfcheckoutmercado.service;
 
-import com.isaac.br.selfcheckoutmercado.dto.CheckoutDTO;
+import com.isaac.br.selfcheckoutmercado.dto.CartItemDTO;
 import com.isaac.br.selfcheckoutmercado.dto.CheckoutResponseDTO;
+import com.isaac.br.selfcheckoutmercado.dto.ResponseCartItem;
 
 public interface CheckoutService {
 
     CheckoutResponseDTO createCheckout();
     void cancelCheckout(long checkoutId);
-
+    ResponseCartItem addItemToCart(Long idSession, CartItemDTO dto);
 }
