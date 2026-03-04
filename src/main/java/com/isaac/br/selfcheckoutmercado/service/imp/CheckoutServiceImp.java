3f6a1015a9f8660c875f23a7aa8dab0e4f9639b4
Isaac <isaac.silva1478@gmail.com>
2026-03-03 22:35:34 -0300

    feat(CheckoutServiceImp): Implementando o metodo para addIteCart

--- src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/CheckoutServiceImp.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/CheckoutServiceImp.java
@@ -1,11 +1,17 @@
 package com.isaac.br.selfcheckoutmercado.service.imp;
 
+import com.isaac.br.selfcheckoutmercado.dto.CartItemDTO;
 import com.isaac.br.selfcheckoutmercado.dto.CheckoutResponseDTO;
+import com.isaac.br.selfcheckoutmercado.dto.ResponseCartItem;
 import com.isaac.br.selfcheckoutmercado.enums.Status;
 import com.isaac.br.selfcheckoutmercado.exceptions.NotFoundException;
+import com.isaac.br.selfcheckoutmercado.model.CartItem;
 import com.isaac.br.selfcheckoutmercado.model.CheckoutSession;
+import com.isaac.br.selfcheckoutmercado.model.Product;
 import com.isaac.br.selfcheckoutmercado.repository.SessionRepository;
+import com.isaac.br.selfcheckoutmercado.service.CartItemService;
 import com.isaac.br.selfcheckoutmercado.service.CheckoutService;
+import com.isaac.br.selfcheckoutmercado.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +23,12 @@ public class CheckoutServiceImp implements CheckoutService {
 
     @Autowired
     private SessionRepository sessionRepository;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CartItemService cartItemService;
 
     @Override
     public CheckoutResponseDTO createCheckout() {
@@ -45,6 +57,37 @@ public class CheckoutServiceImp implements CheckoutService {
             sessionRepository.delete(session.get());
         }catch (Exception e){
             throw new RuntimeException("Erro ao deletar checkout");
+        }
+    }
+
+    @Override
+    public ResponseCartItem addItemToCart(Long idSession, CartItemDTO dto) {
+        Optional<CheckoutSession> checkoutSession = sessionRepository.findById(Math.toIntExact(idSession));
+        if(checkoutSession.isEmpty()){
+            throw new NotFoundException("It was not possible to find checkout");
+        }
+        Product product = productService.getBarCode(dto.barcode());
+        double calc = 0;
+        if (dto.weight() != 0){
+            calc = dto.weight() * product.getPrice();
+        }else{
+            calc = dto.quantity() * product.getPrice();
+        }
+
+        double calcAmount = checkoutSession.get().getTotalAmount() + calc;
+        checkoutSession.get().setTotalAmount(calcAmount);
+        CartItem cartItem = new CartItem();
+        cartItem.setQuantity(dto.quantity());
+        cartItem.setSubtotal(calcAmount);
+        cartItem.setSessionId(checkoutSession.get().getId());
+        cartItem.setProductId(product);
+        try {
+            sessionRepository.save(checkoutSession.get());
+            cartItemService.saveCartItem(cartItem);
+            return new ResponseCartItem(product.getId(),product.getName(), calc,calcAmount);
+
+        }catch (Exception e){
+            throw new RuntimeException("Erro ao salvar checkout");
         }
     }
 }
