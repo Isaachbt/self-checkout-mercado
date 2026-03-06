2363d5dbd2de7ac67e4b428fc24019e7f183269d
Isaac <isaac.silva1478@gmail.com>
2026-03-06 20:48:02 -0300

    feat(NegadoException): exception para validar erros do tipo pin ou cracha invalido

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/exceptions/NegadoException.java
@@ -0,0 +1,7 @@
+package com.isaac.br.selfcheckoutmercado.exceptions;
+
+public class NegadoException extends RuntimeException {
+    public NegadoException(String message) {
+        super(message);
+    }
+}
