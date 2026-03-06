7a9cdbccc828632840a053a27dcf8e2276cfae8b
Isaac <isaac.silva1478@gmail.com>
2026-03-06 20:50:59 -0300

    doc(data.sql): insert para totem e funcionaro automaticamente

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,3 +15,32 @@ INSERT INTO product (name, bar_code, price, price_type) VALUES
 ('Manteiga 200g', '7891001301300', 9.79, 'UNIT'),
 ('Queijo Mussarela', '7891001409400', 34.90, 'WEIGHT'),
 ('Refrigerante Cola 2L', '7891011501509', 8.99, 'UNIT');
+
+
+INSERT INTO terminal (id, terminal_code, active)
+VALUES ('7c8e6f7e-8c4c-4c5c-9d2e-1d4c92b4d111','TOTEM-01', true);
+
+INSERT INTO terminal (id,terminal_code, active)
+VALUES ('3e3c45c9-9e21-4b3a-88a5-9e2a1e1e2222','TOTEM-02', true);
+
+INSERT INTO employee (id,badge_id, name, pin_hash, role, active, failed_attempts)
+VALUES (
+           '2d1a7a1b-8e1a-4a52-9f42-9c4b1a1a3333',
+           1001,
+           'Carlos Silva',
+           '$2a$10$X5e.uag5s37yia2QinBAb.BWd09F6wl9ym2tNY3Up0N4tfa9osI9e',
+           'OPERATOR',
+           true,
+           0
+       );
+
+INSERT INTO employee (id,badge_id, name, pin_hash, role, active, failed_attempts)
+VALUES (
+           '9b8f2a3d-2e1f-4d1e-9a7b-2b1c44444444',
+           1002,
+           'Ana Costa',
+           '$2a$10$7QJ2hXKc6JkL8WgX5m2xUuM1HcO7x3c2d3HhV0t6pOq8zRk9d9X3e',
+           'OPERATOR',
+           true,
+           0
+       );
