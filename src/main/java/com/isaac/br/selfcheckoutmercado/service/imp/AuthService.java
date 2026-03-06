f06504e56d102f4602718b9c0e309d619c4796c3
Isaac <isaac.silva1478@gmail.com>
2026-03-06 20:44:46 -0300

    feat(AuthService): metodo para validar e fazer authentication no totem

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/imp/AuthService.java
@@ -0,0 +1,77 @@
+package com.isaac.br.selfcheckoutmercado.service.imp;
+
+import com.isaac.br.selfcheckoutmercado.dto.LoginDTO;
+import com.isaac.br.selfcheckoutmercado.dto.LoginResponseDTO;
+import com.isaac.br.selfcheckoutmercado.exceptions.NegadoException;
+import com.isaac.br.selfcheckoutmercado.exceptions.NotFoundException;
+import com.isaac.br.selfcheckoutmercado.model.Employee;
+import com.isaac.br.selfcheckoutmercado.model.Terminal;
+import com.isaac.br.selfcheckoutmercado.repository.EmployeeRepository;
+import com.isaac.br.selfcheckoutmercado.repository.TerminalRepository;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private TerminalRepository terminalRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AudiService audiService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public LoginResponseDTO authenticate(LoginDTO loginDTO) {
+        Employee employee = employeeRepository
+                .findByBadgeId(loginDTO.badgeId())
+                .orElseThrow(() -> new NotFoundException("Employee not found"));
+
+        if (!employee.isActive()) {
+            throw new NegadoException("Access denied");
+        }
+
+        if (employee.getLockedUntil() != null &&
+            employee.getLockedUntil().isAfter(LocalDateTime.now())) {
+            throw new NegadoException("Account blocked");
+        }
+
+        System.out.println("=========="+passwordEncoder.matches("1234", employee.getPinHash()));
+
+        if (!passwordEncoder.matches(loginDTO.pin(), employee.getPinHash())){
+            employee.setFailedAttempts(employee.getFailedAttempts() + 1);
+
+            if (employee.getFailedAttempts() >= 5) {
+                employee.setLockedUntil(LocalDateTime.now().plusMinutes(10));
+                employee.setFailedAttempts(0);
+            }
+            employeeRepository.save(employee);
+            throw new NegadoException("PIN invalid");
+        }
+
+        employee.setFailedAttempts(0);
+        employeeRepository.save(employee);
+
+        Terminal terminal = terminalRepository.findByTerminalCode(loginDTO.terminal_code())
+                .orElseThrow(() -> new NotFoundException("Terminal not found"));
+
+        String token = jwtService.generateToken(employee,terminal.getId());
+        audiService.log(employee.getId(),terminal.getId(),"LOGIN");
+        return new LoginResponseDTO(token);
+
+    }
+
+
+}
