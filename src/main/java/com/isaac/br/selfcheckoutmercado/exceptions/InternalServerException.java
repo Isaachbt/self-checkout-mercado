42859e0842a438cdd1a0f754e9be66dace2b18c3
Isaac <isaac.silva1478@gmail.com>
2026-03-08 19:14:51 -0300

    feat(InternalServerException): exception para erros do catch()

---
+++ src/main/java/com/isaac/br/selfcheckoutmercado/exceptions/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.isaac.br.selfcheckoutmercado.exceptions;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
