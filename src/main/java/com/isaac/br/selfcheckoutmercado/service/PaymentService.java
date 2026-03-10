f652bb19bce0f44b3e2ce86e80fd12b9ec9b092f
Isaac <isaac.silva1478@gmail.com>
2026-03-09 22:24:34 -0300

    feat(PaymentService): metodo criado processPayment

--- src/main/java/com/isaac/br/selfcheckoutmercado/service/PaymentService.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/PaymentService.java
@@ -1,4 +1,9 @@
 package com.isaac.br.selfcheckoutmercado.service;
 
+import com.isaac.br.selfcheckoutmercado.dto.MethodPaymentDTO;
+import com.isaac.br.selfcheckoutmercado.dto.PaymentResponse;
+
 public interface PaymentService {
+    PaymentResponse processPayment(long idSession, MethodPaymentDTO paymentDto);
+
 }
