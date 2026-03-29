6ca93ca5b184445b12d5029be4d9d3f003057d9c
Isaac <isaac.silva1478@gmail.com>
2026-03-29 17:02:55 -0300

    feat(CheckoutService): cancel precisa de dois id passado para continuar

--- src/main/java/com/isaac/br/selfcheckoutmercado/service/CheckoutService.java
+++ src/main/java/com/isaac/br/selfcheckoutmercado/service/CheckoutService.java
@@ -5,10 +5,12 @@ import com.isaac.br.selfcheckoutmercado.dto.CheckoutResponseDTO;
 import com.isaac.br.selfcheckoutmercado.dto.ResponseCartItem;
 import com.isaac.br.selfcheckoutmercado.model.CheckoutSession;
 
+import java.util.UUID;
+
 public interface CheckoutService {
 
     CheckoutResponseDTO createCheckout();
-    void cancelCheckout(long checkoutId);
+    void cancelCheckout(long checkoutId, UUID logId);
     ResponseCartItem addItemToCart(Long idSession, CartItemDTO dto);
     void removeItemFromCart(long idCart,long idSession,long productId);
     CheckoutSession getSessionById(Long idSession);
